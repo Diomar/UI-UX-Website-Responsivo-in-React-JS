90e04e8e183266844ffe64f02dd85b1a5c4acce2
Diomar <diomarpr@gmail.com>
2022-04-08 23:40:48 -0300

    Criação dos estilos, cores, gradientes e @media

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Article, Brand, CTA, Feature, Navbar } from './components'
 import { Blog, Fatures, Footer, Header, Possibility, whatGPT3 } from './containers'
 import Features from './containers/features/Features'
 import WhatGPT3 from './containers/whatGPT3/WhatGPT3'
-
+import './App.css'
 
 const App = () => {
   return (
